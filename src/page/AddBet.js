055e20e634747f576356bf5c50b9bc56fe3b5a98
Christian Nielsen <chrnielsen2003@gmail.com>
2022-10-31 12:48:48 +0100

    addbet

--- src/page/AddBet.js
+++ src/page/AddBet.js
@@ -1,9 +1,32 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 
 export default function AddBet() {
   const navigate = useNavigate();
+  const [kupon, setKupon] = useState([]);
+  const [indskud, setIndskud] = useState(1);
+  const [leagues, setLeagues] = useState([]);
+
+  useEffect(() => {
+    axios.get("/ligaer.json").then((data) => console.log(data.data));
+  }, []);
+
+  function totalOddsCalc(e) {
+    var totalOdds = 1;
+    var oddsArray = [];
+
+    kupon.map((e) => {
+      return oddsArray.push(e.odds);
+    });
+
+    oddsArray.forEach((e) => {
+      return (totalOdds = totalOdds * e);
+    });
+    console.log(totalOdds.toFixed(2));
+    return totalOdds.toFixed(2);
+  }
 
   useEffect(() => {
     if (auth?.currentUser.email === "nstaalkjaer@gmail.com") {
@@ -24,20 +47,175 @@ export default function AddBet() {
       }
     }
   }, [navigate]);
+
+  function addMatchBet(e) {
+    e.preventDefault();
+    const datoForKamp = e.target.date.value;
+    const finalOdds = e.target.finalOdds.value.replace(",", ".");
+    var matchOdds = {
+      odds: parseFloat(finalOdds),
+      dato: dateConverter(datoForKamp),
+      home: e.target.home.value,
+      away: e.target.away.value,
+      play: e.target.odds.value,
+      league: e.target.league.value,
+    };
+
+    setKupon((oldarray) => [...oldarray, matchOdds]);
+  }
+
+  function dateConverter(e) {
+    const t = e.split("-");
+    return `${t[2]}-${t[1]}/${t[0]}`;
+  }
+
+  function playKupon(e) {
+    e.preventDefault();
+  }
+
   return (
     <div>
       <Link to={"/adminPanel"}>Tilbage</Link>
       <h1>Tilføj bet</h1>
 
-      <form>
+      <form onSubmit={(e) => addMatchBet(e)}>
+        <div>
+          <label htmlFor="date">Vælg dato for kamp</label>
+          <input required type="date" name="date" />
+        </div>
         <div>
           <label htmlFor="liga">Liga:</label>
-          <select>
-            <option value=""></option>
-            <option value="CL">Champions league</option>
-          </select>
+          <input required list="leagueList" name="league" />
+
+          <datalist id="leagueList">
+            <option value="Premier League" id="1"></option>
+            <option value="Bundesliga" id="2"></option>
+            <option value="La Liga" id="3"></option>
+            <option value="Ligue 1" id="4"></option>
+            <option value="Champions league" id="5"></option>
+            <option value="conferense league" id="6"></option>
+            <option value="europa league" id="7"></option>
+          </datalist>
+
+          <div>
+            <label htmlFor="home">Hjemmehold:</label>
+            <input required type="text" name="home" />
+          </div>
+          <div>
+            <label htmlFor="away">Udehold:</label>
+            <input required type="text" name="away" />
+          </div>
+
+          <div>
+            <label htmlFor="odds">Hvad skal vi oddse?</label>
+            <textarea
+              required
+              name="odds"
+              id="odss"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+
+          <div>
+            <label htmlFor="value">Hvad er oddsen på den kamp?</label>
+            <input required name="finalOdds" type={"tel"} />
+          </div>
         </div>
+
+        <input type="submit" value="tilføj til kupon" />
       </form>
+
+      <form onSubmit={(e) => playKupon(e)}>
+        <p>Kupon</p>
+        <div
+          style={{
+            backgroundColor: "grey",
+            width: "75%",
+            minHeight: "100px",
+            margin: "0 auto",
+          }}
+        >
+          <p>Antal kampe: {kupon.length}</p>
+          <div>
+            <label htmlFor="indskud">Indskud</label>
+            <input
+              type="number"
+              name="indskud"
+              onBlur={(e) => setIndskud(e.target.value)}
+            />
+          </div>
+          {kupon?.map((e) => {
+            return (
+              <>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    textAlign: "center",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      flex: "10",
+                    }}
+                  >
+                    <p key={e.home}>{e.home}</p>
+                    <p> - </p>
+                    <p key={e.away}>{e.away}</p>
+                  </div>
+                  <div style={{ flex: "9" }}>
+                    <p>{e.odds}</p>
+                  </div>
+                </div>
+                <span
+                  style={{
+                    fontSize: "0.9em",
+                    position: "relative",
+                    right: "90px",
+                    bottom: "20px",
+                  }}
+                >
+                  {e.play}
+                </span>
+              </>
+            );
+          })}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginRight: "4em",
+            }}
+          >
+            <p>Total odds:</p>
+            <p>{totalOddsCalc()}</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginRight: "4em",
+            }}
+          >
+            <p>Potentiel gevinst:</p>
+            <p>{totalOddsCalc() * indskud} kr</p>
+          </div>
+        </div>
+
+        <input type="submit" value="Spil kupon" />
+      </form>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
